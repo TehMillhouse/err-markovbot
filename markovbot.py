3bd2f7eba1ed80c1052c5fd019882ee018abb5bd
Max Wagner <max.wagner01@gmail.com>
2012-11-10 17:30:44 +0100

    Only create markov chain once, then reuse it.
    This vastly improves the performance of the bot.

--- markovbot.py
+++ markovbot.py
@@ -7,34 +7,36 @@ import requests
 
 class MarkovBot(BotPlugin):
 
+	def __init__(self):
+		self.markov = MarkovChain()
+
     @botcmd
     def talk(self, mess, args):
         """ Generate a sentence based on database """
-        return MarkovChain().generateString()
+        return self.markov.generateString()
 
     @botcmd
     def complete(self, mess, args):
         """ Try to complete a sentence """
-        return MarkovChain().generateStringWithSeed(args)
+        return self.markov.generateStringWithSeed(args)
 
     @botcmd
     def dbgenfromfile(self, mess, args):
         """ Generate markov chain word database """
-        markov = MarkovChain()
         try:
             with open(args) as txtFile:
                 txt = txtFile.read()
         except IOError as e:
             return 'Error: could not open text file'
         # At this point, we've got the file contents
-        if markov.generateDatabase(txt):
+        if self.markov.generateDatabase(txt):
             return 'Done.'
         else:
             return 'Error: Could not generate database'
 
     @botcmd
     def dbgenfromstring(self, mess, args):
-        if MarkovChain().generateDatabase(args):
+        if self.markov.generateDatabase(args):
             return 'Done.'
         else:
             return 'Error: Could not generate database from String'
@@ -42,7 +44,7 @@ class MarkovBot(BotPlugin):
     @botcmd
     def dbgenfromurl(self, mess, args):
         req = requests.get(args)
-        if req.ok and MarkovChain().generateDatabase(req.content):
+        if req.ok and self.markov.generateDatabase(req.content):
             return 'Done.'
         else:
             return 'Error: Could not generate database from URL'
