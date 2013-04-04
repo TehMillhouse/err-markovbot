242f93bc608d73ce20cc2581390eee03fd39b202
Max Wagner <max@trollbu.de>
2013-04-04 17:55:48 +0200

    Let users specify sentence separators

--- markovbot.py
+++ markovbot.py
@@ -9,6 +9,7 @@ class MarkovBot(BotPlugin):
 
     def __init__(self):
         super(MarkovBot, self).__init__()
+        self.sentenceSep = None
         self.markov = MarkovChain(dbFilePath='./markovdb')
 
     @botcmd
@@ -30,15 +31,28 @@ class MarkovBot(BotPlugin):
         except IOError as e:
             return 'Error: could not open text file'
         # At this point, we've got the file contents
-        if self.markov.generateDatabase(txt):
+        if self.sentenceSep:
+            result = self.markov.generateDatabase(txt, self.sentenceSep)
+        else:
+            result = self.markov.generateDatabase(txt)
+        if result:
             return 'Done.'
         else:
             return 'Error: Could not generate database'
 
     @botcmd
+    def setsentencesep(self, mess, args):
+        """ Specify how to detect sentence borders """
+        self.sentenceSep = args
+
+    @botcmd
     def gendbfromstring(self, mess, args):
         """ Generate markov chain word database based on given string """
-        if self.markov.generateDatabase(args):
+        if self.sentenceSep:
+            result = self.markov.generateDatabase(args, self.sentenceSep)
+        else:
+            result = self.markov.generateDatabase(args)
+        if result:
             return 'Done.'
         else:
             return 'Error: Could not generate database from String'
@@ -47,7 +61,12 @@ class MarkovBot(BotPlugin):
     def gendbfromurl(self, mess, args):
         """ Generate markov chain word database based on contents of url """
         response, content = httplib2.Http().request(args, 'GET')
-        if response['status'] == '200' and self.markov.generateDatabase(content.decode('utf-8')):
-            return 'Done.'
-        else:
-            return 'Error: Could not generate database from URL'
+        if response['status'] == '200':
+            if self.sentenceSep:
+                result = self.markov.generateDatabase(content.decode('utf-8'), self.sentenceSep)
+            else:
+                result = self.markov.generateDatabase(content.decode('utf-8'))
+            if result:
+                return 'Done.'
+            else:
+                return 'Error: Could not generate database from URL'
